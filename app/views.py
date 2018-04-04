1c1cb18b5218f6b759bf5156859f08e51739d26b
garvinipkiss <garvinipkiss@gmail.com>
2018-04-04 16:06:43 +0300

    first commit

--- app/views.py
+++ app/views.py
@@ -2,9 +2,11 @@ from flask import render_template
 from app import app
 
 @app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+def index():
 
     '''
-    View movie page function that returns the movie details page and its data
+    View root page function that returns the index page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
