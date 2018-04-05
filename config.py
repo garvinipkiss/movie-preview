7729af5f3a0187fb24ed19fdf8770e5bd0608940
garvinipkiss <garvinipkiss@gmail.com>
2018-04-05 12:45:51 +0300

    initial changes

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vincent:12345@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS= False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'Pruduction':ProdConfig
+}
